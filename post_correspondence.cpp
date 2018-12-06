8401370dd04d8ef2847f7e5fe7830267385ad205
Michael Quinn <mquinn5@binghamton.edu>
2018-12-06 13:26:29 -0500

    added some stuff

--- post_correspondence.cpp
+++ post_correspondence.cpp
@@ -77,6 +77,44 @@ int main(){
 			top = top + dominoes[num-1].getTop();
 			bottom = bottom + dominoes[num-1].getBottom();
 		}
+		bool matching = top.length()==bottom.length();
+		for(unsigned int i=0; i<top.length && i<bottom.length; i++){
+			if(top[i]!=bottom[i]){
+				matching = false;
+				cout << "Doesn't match:" << endl;
+				for(unsigned int j=0; j<top.length; j++){
+					if(j>=bottom.length() || top[j]=bottom[i])
+						cout << top[j];
+					else{
+						if(j==0 || top[j-1]==bottom[j-1])
+							cout << "<";
+						cout << top[j];
+						if(j+1>=top.length() || j+1>=bottom.length() || top[j+1]==bottom[j+1])
+							cout << ">";
+					}
+				}
+				cout << endl;
+				for(unsigned int j=0; j<bottom.length; j++){
+					if(j>=top.length() || top[j]=bottom[i])
+						cout << bottom[j];
+					else{
+						if(j==0 || top[j-1]==bottom[j-1])
+							cout << "<";
+						cout << bottom[j];
+						if(j+1>=top.length() || j+1>=bottom.length() || top[j+1]==bottom[j+1])
+							cout << ">";
+					}
+				}
+				top="";
+				bottom="";
+				cout << "Reset top and bottom" << endl;
+				break;
+			}
+		}
+		if(matching){
+			cont = false;
+			cout << "Top and Bottom Match!\n\"" << top << "\"\n\"" << bottom << "\"" << endl;
+		}
 	} while (cont);
 	
 	return 0;
